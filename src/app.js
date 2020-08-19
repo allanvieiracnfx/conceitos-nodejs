f564c34e671271b943a9b8b384c9474d9e370d2c
Allan Vieira <allan.vieira@dataa.com.br>
2020-08-19 03:12:35 -0300

    Conceitos NodeJS - Project CRUD Repository, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid, v4: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,78 @@ app.use(cors());
 
 const repositories = [];
 
+
+function validateRepositoryId(request, response, next){
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).send()
+  }
+
+  return next();
+}
+
+function validatedRepository(request, response, next){
+  const { id } = request.params;
+
+  const index = getIndexRepository(id);
+
+  if(index < 0){
+    return  response.status(400).send();
+  }
+
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId, validatedRepository);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const repository = getNewRepository(request, 0, 0);
+  repositories.push(repository);
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = getIndexRepository(id);
+  const likes = repositories[index].likes;
+  repositories[index] = getNewRepository(request, id, likes);
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = getIndexRepository(id);
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = getIndexRepository(id);
+  const repository = repositories[index];
+  repository.likes += 1;
+  return response.json(repository);
 });
+
+function getIndexRepository(id){
+  return repositories.findIndex(repository => repository.id === id);
+}
+
+function getNewRepository(request, id, likes){
+  const { title, url, techs } = request.body;
+  id = id > 0 ? id : uuid();
+  return repository = {
+    id,
+    url,
+    title,
+    techs,
+    likes
+  };
+}
 
 module.exports = app;
